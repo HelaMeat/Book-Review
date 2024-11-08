43fc419a2aa2937757304991aa27548011063146
HelaMeat <sameerbobby19@gmail.com>
2024-11-08 23:40:09 +0530

    Test Commit

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('sammmdd')
+from fastapi import FastAPI,Header
+from typing import Optional, List
+from schemas import BookCreateModel
+
+app = FastAPI()
+
+@app.get('/')
+async def get():
+    return {'message': 'Hello, World!'}
+
+@app.get('/greet')
+async def get_ded(name:Optional[str] = "user" , age:int = 24) -> dict:
+    return {'message': f"This is a {name}" , "age":age}
+
+@app.post('/create_book')
+async def create_book(book_data:BookCreateModel):
+    return {'title' : book_data.title, 
+            'author' : book_data.author , 
+            'publication_year' : book_data.publication_year}    
+    
+@app.get('/books', response_model=List[BookCreateModel])
+async def get_books(book_data:BookCreateModel) -> BookCreateModel:
+    return book_data
+
+@app.get('/get_header')
+async def getHead(accept:str = Header(None)):
+    request_headers = {}
+    
+    request_headers['Accept'] = accept
+    
+    return request_headers
+    
+    
