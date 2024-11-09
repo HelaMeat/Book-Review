ea7da23e3a88ea2e4064087481d4b8bf7112e1e2
HelaMeat <sameerbobby19@gmail.com>
2024-11-10 00:25:14 +0530

    db test

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+# This context manager is used to initialize the database and close when the server stops.
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"server starting")
+    await init_db()
+    yield 
+    print(f"server stopped")
 
 version = 'v1'
 
 app = FastAPI(
+    lifespan=life_span,
     version = version
 )
 
